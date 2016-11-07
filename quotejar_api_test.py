323dbf2bf16dcfe9efad38c53a38c647f701319a
BRAUER <bbbrau@monsanto.com>
2016-11-06 19:47:11 -0600

    finish api work

---
+++ quotejar_api_test.py
@@ -0,0 +1,95 @@
+import requests
+import simplejson as json
+
+base_url = 'http://localhost:3000/api/'
+
+headers = {'Content-Type': 'application/json'}
+
+print("Testing post users with bad username and password")
+payload = {
+	"email": "bademail",
+	"password": ""
+}
+url = base_url + 'userID'
+user = requests.post(url, headers=headers, data=json.dumps(payload))
+print(user.content + '\n')
+
+print("Testing post userID with good username and password")
+payload = {
+	'email': 'tester@test.com',
+	'password': 'password'
+}
+user = requests.post(url, headers=headers, data=json.dumps(payload))
+user_json = json.loads(user.content)
+user_id = user_json[0]["_id"]
+print(user_id)
+print(user.content + '\n')
+
+print("Getting tags")
+url = base_url + 'tags'
+tags = requests.get(url, headers=headers)
+print(tags.content + '\n')
+
+print("Post query for current users quotes")
+url = base_url + 'quotes'
+post_url = url + '/query'
+query = {
+	"user_id": user_id
+}
+user_quotes = requests.post(post_url, headers=headers, data=json.dumps(query))
+print(user_quotes.content + '\n')
+
+print("Posting quote with tags outside index range")
+payload = {
+	"user_id": user_id,
+	"tags": [10],
+	"isFavorite": "true",
+	"rating": 5,
+	"text": "this is just a test",
+	"said_by": "a tester"	
+}
+bad_tag_quote = requests.post(url, headers=headers, data=json.dumps(payload))
+print(bad_tag_quote.status_code)
+print(bad_tag_quote.content + '\n')
+
+print("Posting quote with bad user id")
+payload["tags"] = [1,2]
+payload["user_id"] = "1234567890ab"
+bad_user_quote = requests.post(url, headers=headers, data=json.dumps(payload))
+print(bad_user_quote.status_code)
+print(bad_user_quote.content + '\n')
+
+print("Posting good quote")
+payload["user_id"] = user_id
+print(payload)
+good_quote = requests.post(url, headers=headers, data=json.dumps(payload))
+print(good_quote.content + '\n')
+
+print("Updated quotes list:")
+user_quotes = requests.post(post_url, headers=headers, data=json.dumps(query))
+print(user_quotes.content + '\n')
+
+print("Updating quote content")
+put_query = json.loads(good_quote.content)
+put_query["text"] = "this is some updated text"
+update_result = requests.put(url, headers=headers, data=json.dumps(put_query))
+print('Update response: ' + str(update_result) + ', ' + str(update_result.content) + '\n')
+
+print("Updated quotes list:")
+user_quotes = requests.post(post_url, headers=headers, data=json.dumps(query))
+print(user_quotes.content + '\n')
+
+print("Deleting quote")
+delete_query = {
+	"_id": put_query["_id"]
+}
+delete_result = requests.delete(url, headers=headers, data=json.dumps(delete_query))
+print('Delete response: ' + str(delete_result) + '\n')
+
+print("Quotes list after delete:")
+user_quotes = requests.post(post_url, headers=headers, data=json.dumps(query))
+print(user_quotes.content + '\n')
+
+
+
+
--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,7 @@ router.get('/tags', function(req, res, next) {
 
 router.post('/userID', function(req, res, next) {
 	//validate stuff posted in body
+	console.log(req.body)
 	req.checkBody("email", "Please Enter a Valid Email Address").isEmail();
 	req.checkBody("password", "Please enter a valid password").notEmpty();
 	var errors = req.validationErrors();
@@ -24,14 +25,13 @@ router.post('/userID', function(req, res, next) {
 	users.find({email: req.body.email, password: req.body.password}, {}, function(e, user) {
 		if (errors) {
 			//bad email and/or password sent in
-			res.status = 400;
+			res.status(400);
 			res.send(errors);
 		} else if (user.length > 0) {
 			//return json of user info
 			res.json(user);
 		} else {
 			//user doesn't exist
-			res.status = 200;
 			res.send('[]');
 		}
 	});
@@ -68,11 +68,136 @@ router.post('/quotes/query', function(req, res, next) {
 	});
 });
 
+//delete quote
+router.delete('/quotes', function(req, res) {
+	var db = req.db;
+	var quotes = db.get('quoteCollection');
+	quotes.remove({_id: req.body._id}, {}, function(e, result) {
+		if (e) {
+			res.status(400);
+			res.send("Quote id could not be deleted");
+		} else {
+			res.json(result);
+		}
+	});
+});
+
+//edit quote
+router.put('/quotes', function(req, res, next) {
+	var db = req.db;
+	var quotes = db.get('quoteCollection');
+	var tags = db.get('tag');
+	var users = db.get('userCollection');
+
+	//validate input
+	req.checkBody("text", "Please enter some quote text").notEmpty();
+	req.checkBody("said_by", "Please tell us who said the quote").notEmpty();
+	req.checkBody("isFavorite", "Please make sure isFavorite is a boolean value").isBoolean();
+	req.checkBody("rating", "Please make sure rating is valid value between 1 and 9").isInt({"max": 9, "min": 1});
+	req.checkBody("_id", "Please enter a quote id").notEmpty();
+	var errors = req.validationErrors();
+
+	//validate that user_id exists
+	var user_ok = true;
+	var user_id = "";
+	//validate that tags exist
+	var tags_ok = true;
+	var my_tags = [];
+
+	users.find({"_id": mongo.ObjectID(req.body.user_id)}, {}, function(e, user_json) {
+		if (user_json.length > 0) {
+			console.log(user_json);
+			user_id = user_json[0]["_id"];
+		} else {
+			user_ok = false;
+		}
+		if (req.body.tags) {
+			async.eachSeries(req.body.tags, function(item, callback) {
+				console.log(item);
+				tags.find({_id: parseInt(item)}, {}, function(e, tag) {
+					console.log(tag);
+					if (tag.length > 0) {
+						console.log("Tag found");
+						my_tags.push(item);
+					}
+					else {
+						console.log("Tag not found");
+						tags_ok = false;
+					}
+					callback(e);
+				});
+			}, function(err) {
+				if (errors) {
+					res.status(400);
+					res.send(errors);
+				} else if (!tags_ok) {
+					res.status(400);	
+					res.send("Tags don't exist for all ids in tags array");
+				} else if (!user_ok) {
+					res.status(400);
+					res.send("User id does not exist");
+				} else {
+					var myQuery = {};
+					//create new quote and return result
+					if (req.body.text) myQuery.text = req.body.text;
+					if (req.body.said_by) myQuery.said_by = req.body.said_by;
+					if (req.body.isFavorite) myQuery.isFavorite = req.body.isFavorite;
+					myQuery.tags = my_tags;
+					if (req.body.user_id) myQuery.user_id = req.body.user_id;
+					console.log(myQuery);
+					quotes.update({"_id": mongo.ObjectID(req.body._id)}, myQuery, function(e, doc) {
+						if (e) {
+							console.warn(message);
+						} else {
+							console.log(doc);
+							quotes.find({"_id": mongo.ObjectID(req.body._id)}, {}, function(e, quote_json) {
+								res.json(quote_json);
+							});
+						}
+					})
+				}
+			});
+		} else {
+			if (errors) {
+				res.status(400);
+				res.send(errors);
+			} else if (!tags_ok) {
+				res.status(400);
+				res.send("Tags don't exist for all ids in tags array");
+			} else if (!user_ok) {
+				res.status(400);
+				res.send("User id does not exist");
+			} else {
+				var myQuery = {};
+				//create new quote and return result
+				if (req.body.text) myQuery.text = req.body.text;
+				if (req.body.said_by) myQuery.said_by = req.body.said_by;
+				if (req.body.isFavorite) myQuery.isFavorite = req.body.isFavorite;
+				if (req.body.rating) myQuery.rating = req.body.rating;
+				myQuery.tags = my_tags;
+				if (req.body.user_id) myQuery.user_id = req.body.user_id;
+				console.log(myQuery);
+				quotes.update({"_id": mongo.ObjectID(req.body._id)}, myQuery, function(e, doc) {
+					if (e) {
+						console.warn(message);
+					} else {
+						console.log(doc);
+						quotes.find({"_id": mongo.ObjectID(req.body._id)}, {}, function(e, quote_json) {
+							res.json(quote_json);
+						});
+					}
+				})
+			}
+		}
+	});
+});
+
 //add quote
 router.post('/quotes', function(req, res, next) {
 	var db = req.db;
 	var quotes = db.get('quoteCollection');
 	var tags = db.get('tag');
+	var users = db.get('userCollection');
 
 	//validate input
 	req.checkBody("text", "Please enter some quote text").notEmpty();
@@ -83,35 +208,72 @@ router.post('/quotes', function(req, res, next) {
 
 	//validate that user_id exists
 	var user_ok = true;
-
-
+	var user_id = "";
 	//validate that tags exist
 	var tags_ok = true;
 	var my_tags = [];
-	if (req.body.tags) {
-		async.eachSeries(req.body.tags, function(item, callback) {
-			console.log(item);
-			tags.find({_id: parseInt(item)}, {}, function(e, tag) {
-				console.log(tag);
-				if (tag) {
-					console.log("Tag found");
-					my_tags.push(item);
+
+	users.find({"_id": mongo.ObjectID(req.body.user_id)}, {}, function(e, user_json) {
+		if (user_json.length > 0) {
+			console.log(user_json);
+			user_id = user_json[0]["_id"];
+			console.log(user_id);
+		} else {
+			user_ok = false;
+		}
+		if (req.body.tags) {
+			async.eachSeries(req.body.tags, function(item, callback) {
+				console.log(item);
+				tags.find({_id: parseInt(item)}, {}, function(e, tag) {
+					console.log(tag);
+					if (tag.length > 0) {
+						console.log("Tag found");
+						my_tags.push(item);
+					}
+					else {
+						console.log("Tag not found");
+						tags_ok = false;
+					}
+					callback(e);
+				});
+			}, function(err) {
+				if (errors) {
+					res.status(400);
+					res.send(errors);
+				} else if (!tags_ok) {
+					res.status(400);
+					res.send("Tags don't exist for all ids in tags array");
+				} else if (!user_ok) {
+					res.status(400);
+					res.send("User id does not exist");
+				} else {
+					var myQuery = {};
+					//create new quote and return result
+					if (req.body.text) myQuery.text = req.body.text;
+					if (req.body.said_by) myQuery.said_by = req.body.said_by;
+					if (req.body.isFavorite) myQuery.isFavorite = req.body.isFavorite;
+					if (req.body.rating) myQuery.rating = req.body.rating;
+					myQuery.tags = my_tags;
+					if (req.body.user_id) myQuery.user_id = req.body.user_id;
+					console.log(myQuery);
+					quotes.insert(myQuery, function(e, doc) {
+						if (e) {
+							console.warn(message);
+						} else {
+							res.json(doc);
+						}
+					})
 				}
-				else {
-					console.log("Tag not found");
-					tags_ok = false;
-				}
-				callback(e);
 			});
-		}, function(err) {
+		} else {
 			if (errors) {
-				res.status = 400;
+				res.status(400);
 				res.send(errors);
 			} else if (!tags_ok) {
-				res.status = 400;
+				res.status(400);
 				res.send("Tags don't exist for all ids in tags array");
 			} else if (!user_ok) {
-				res.status = 400;
+				res.status(400);
 				res.send("User id does not exist");
 			} else {
 				var myQuery = {};
@@ -131,56 +293,10 @@ router.post('/quotes', function(req, res, next) {
 					}
 				})
 			}
-		});
-	} else {
-		if (errors) {
-			res.status = 400;
-			res.send(errors);
-		} else if (!tags_ok) {
-			res.status = 400;
-			res.send("Tags don't exist for all ids in tags array");
-		} else if (!user_ok) {
-			res.status = 400;
-			res.send("User id does not exist");
-		} else {
-			var myQuery = {};
-			//create new quote and return result
-			if (req.body.text) myQuery.text = req.body.text;
-			if (req.body.said_by) myQuery.said_by = req.body.said_by;
-			if (req.body.isFavorite) myQuery.isFavorite = req.body.isFavorite;
-			if (req.body.rating) myQuery.rating = req.body.rating;
-			myQuery.tags = my_tags;
-			if (req.body.user_id) myQuery.user_id = req.body.user_id;
-			console.log(myQuery);
-			quotes.insert(myQuery, function(e, doc) {
-				if (e) {
-					console.warn(message);
-				} else {
-					res.json(doc);
-				}
-			})
-		}
-	}
-});
-
-var tag_exists = function(tag_id) {
-	var tags = myDb.get('tag');
-	console.log(tag_id);
-	tags.find({_id: parseInt(tag_id)}, {}, function(e, tag) {
-		console.log(tag);
-		if (tag) {
-			console.log("Tag found");
-			return true;
-		}
-		else {
-			console.log("Tag not found");
-			return false;
 		}
 	});
-}
+});
 
-var user_exists = function(user_id, db) {
 
-}
 
 module.exports = router;
